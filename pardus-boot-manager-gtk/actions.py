ac3c0e387a78a37d70ee194d2231a0c5a8f1bef4
Rıdvan Örsvuran <flasherdn@gmail.com>
2010-02-06 21:05:51 +0200

    added pardus-boot-manager-gtk

---
+++ pardus-boot-manager-gtk/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# license: gpl v3
+#
+# Rıdvan Örsvuran <flasherdn@gmail.com>
+
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+
+def setup():
+    pass
+
+def build():
+    pass
+
+def install():
+    pythonmodules.install()
+    pisitools.dodoc("README")
+    for lang in ["en",  "tr"]:
+        pisitools.domo("po/%s.po" % lang, lang, "boot_manager_gtk.mo")
+    pisitools.rename("/usr/bin/boot-manager-gtk.py", "boot-manager-gtk")
+    #to avoid file conflicts
+    pisitools.remove("/usr/lib/python2.6/site-packages/asma/addons/__init__.py")
